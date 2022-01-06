5ef857594ca2ce3af602ae4d742b3382905dad07
sam.gray121@gmail.com <Mrtwister123!>
2022-01-05 18:43:41 -0600

    Update random test

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age;
+        int maxHeartRate;
+        int targetHeartRate;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is your age?");
+        age = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Your maximum heart rate is " + (220 - age));
+
+        System.out.println("Your target heart rate zone should be between " + ((220 - age) * .5) + " and " + ((220-age) * .85));
+
+        System.out.println("random");
+
+
+
+
+
+
+
+
+
+    }
+}
